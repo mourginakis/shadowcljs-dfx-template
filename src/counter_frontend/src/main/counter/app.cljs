2780ed607535eee04ab4e17a3c9e576ccd283d3b
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2023-02-12 14:38:45 +0100

    refactor app into components

--- src/counter_frontend/src/main/counter/app.cljs
+++ src/counter_frontend/src/main/counter/app.cljs
@@ -6,34 +6,53 @@
 
 (println "code reloaded!")
 
+
+(defn JS-Library-Demo []
+  [:div [:h3 "JS Library Demo"]
+   "Importing and calling a JS library from ClojureScript:" [:br]
+   [:p "sha256('abc') => " (-> (hashjs/sha256) (.update "abc") (.digest "hex"))]])
+
 (defonce current-count (r/atom 0))
+(defn Counter-Frontend []
+  [:div [:h3 "Counter-Frontend"]
+   "This is a counter that stores state in an atom in the frontend" [:br]
+   "Current Count: " @current-count [:br]
+   [:button {:onClick (fn [] (swap! current-count inc))} "inc"]
+   [:button {:onClick (fn [] (swap! current-count dec))} "dec"]])
+
+
+(defonce whoami-value (r/atom ""))
+(defn WhoAmI-Checker []
+  [:div 
+   "whoami => " @whoami-value [:br]
+   [:button {:onClick
+             (fn [] (reset! whoami-value "loading...") 
+                    (-> (js/Promise.resolve (actor/sanityWhoAmI))
+                        (.then #(reset! whoami-value %))
+                        (.catch #(reset! whoami-value "error"))
+                        (.finally #(js/console.log "whoami call finished"))))} 
+    "fetch"]
+   
+   [:button {:onClick (fn [] (reset! whoami-value ""))} "reset"]
+   ])
+
+
+(defn Counter-Backend []
+  [:div [:h3 "Counter-Backend"] 
+   "Calling the canister" [:br]
+   (WhoAmI-Checker)
+   "js import sanity check => " actor/sanityCheck [:br]
+   "Current Count: " "Not Yet Implemented"])
+
 
 (defn Application []
   [:div
-   [:h1 "A simple counter app!"]
-   [:h3 "Calling a JS library from ClojureScript:"]
-   [:p "sha256('abc') =>" (-> (hashjs/sha256) (.update "abc") (.digest "hex"))]
-
-   [:h3 "Current count:"]
-   [:p @current-count]
-
-   [:button
-    {:onClick (fn [] (swap! current-count inc))}
-    "+1"]
-   [:button
-    {:onClick (fn [] (swap! current-count dec))}
-    "-1"] 
-   
-   [:h3 "Calling the canister"] 
-   [:p "js import sanity check => " actor/sanityCheck] 
-   [:p "whoami => " (str "need to make this update on promise resolve")]
+   [:h1 "A simple counter app!"] [:hr]
+   (JS-Library-Demo) [:hr]
+   (Counter-Frontend) [:hr]
+   (Counter-Backend) [:hr]
    
    ])
-
-;; works, needs to be moved into Application
-(.then (js/Promise.resolve (actor/sanityWhoAmI))
-       #(js/console.log %))
-
 
 
 (dom/render [Application] (js/document.getElementById "app"))
